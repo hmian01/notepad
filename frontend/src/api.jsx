b39ab73954db2e21da7677010755c203d09020e5
hm <hmian5869@gmail.com>
2025-09-28 21:33:38 -0500

    navbar + signin page

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -13,3 +13,14 @@ export async function signup({ name, email, password }) {
   if (!res.ok) throw new Error("signup failed"); 
   return res.json().catch(() => ({}));
 }
+
+export async function signin({ email, password }) {
+  const res = await fetch("/api/users/signin", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!res.ok) throw new Error("signin failed"); 
+  return res.json().catch(() => ({}));
+}
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,9 +1,11 @@
 // src/pages/SignUp.jsx
 import { useState } from "react";
-import { signup } from "../api"; 
+import { signin } from "../api"; 
+import { Link, useNavigate } from "react-router-dom";
+
 import "../index.css";
 
-export default function SignUp() {
+export default function SignIn() {
 
   const [msg, setMsg] = useState(""); // message that will display to user
 
@@ -13,15 +15,13 @@ export default function SignUp() {
     setMsg("");
     const formEl = e.currentTarget;
     const form = new FormData(formEl);
-    const name = form.get("name");
     const email = form.get("email");
     const password = form.get("password");
-    if (!name) return setMsg("name required");
     if (!email || !password) return setMsg("email and password required");
 
     try {
-      await signup({ name, email, password }); // call to api.jsx
-      setMsg("account created ✔");
+      await signin({ email, password }); // call to api.jsx
+      navigate("/dashboard");
       formEl.reset();
     } catch (err) {
       setMsg(err.message);
@@ -32,15 +32,15 @@ export default function SignUp() {
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
     <div className="card w-96 bg-black shadow-xl rounded-2xl">
       <div className="card-body">
-        <h2 className="card-title justify-center">Sign Up</h2>
+        <h2 className="card-title justify-center">Sign In</h2>
         <br/>
         <form className="flex flex-col gap-3" onSubmit={onSubmit}>
-          <input name="name" type="name" placeholder="name" className="input input-bordered w-full"/>
           <input name="email" type="email" placeholder="email" className="input input-bordered w-full"/>
           <input name="password" type="password" placeholder="password" className="input input-bordered w-full"/>
-          <button type="submit" className="btn btn-primary w-full">sign up</button>
           {msg && <p className="text-sm text-red-500">{msg}</p>} 
+          <button type="submit" className="btn btn-primary w-full">sign in</button>
         </form>
+        <p className="text-center"> <br/>New to Notepad? <Link to="/signup" className="text-blue-400 underline">Create Account</Link></p>
       </div>
     </div>
     </div>
