3850443c7aabd1c93e1ed671b68222d801590aa9
hm <hmian5869@gmail.com>
2025-10-18 15:30:12 -0500

    backend api url now held in central location

--- frontend/src/api/authApi.jsx
+++ frontend/src/api/authApi.jsx
@@ -1,17 +1,19 @@
 // api.jsx
 import axios from 'axios';
 
+const BACKEND_API_URL = import.meta.env.VITE_BACKEND_API_URL;
+
 const api = axios.create({
-  baseURL: 'http://localhost:8080/',
+  baseURL: BACKEND_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export function signup(name, email, password) {
-  return api.post('/api/users/signup', { name, email, password });
+  return api.post('/users/signup', { name, email, password });
 }
 
 export function signin(email, password) {
-  return api.post('/api/users/signin', { email, password });
+  return api.post('/users/signin', { email, password });
 }
--- frontend/src/api/notesApi.jsx
+++ frontend/src/api/notesApi.jsx
@@ -1,8 +1,10 @@
 
 import axios from 'axios';
 
+const BACKEND_API_URL = import.meta.env.VITE_BACKEND_API_URL;
+
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: BACKEND_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
