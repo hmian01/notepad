3f91861b89bfc1f198d7ba84fa2345b26462d982
hm <hmian5869@gmail.com>
2025-10-20 03:22:53 -0500

    set buttons to loading to show input has registered

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -14,6 +14,7 @@ export default function SignIn() {
 
   async function onSubmit(e) {
     e.preventDefault();
+    setStatus("loading");
 
     const formEl = e.currentTarget;
     const form = new FormData(formEl);
